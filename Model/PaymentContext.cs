6d66571698ed62a29ba1603329448ffad427e693
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-11 21:38:31 +0530

    Add project files.

---
+++ Model/PaymentContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Model
+{
+    public class PaymentContext : DbContext
+    {
+        public PaymentContext(DbContextOptions<PaymentContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
